259c5636c2998b0e00e18ffc6e1831d50cbaba6d
Snehesh Shrestha <snehesh@umd.edu>
2020-06-28 17:07:50 -0400

    Fixed a lot of issues after running and debugging issues

--- distanceToSimplex.py
+++ distanceToSimplex.py
@@ -1,6 +1,5 @@
 import numpy as np
-from sympy import Matrx
-import random
+from sympy import Matrix
 
 
 def distanceToSimplex(point, S):
@@ -15,31 +14,39 @@ def distanceToSimplex(point, S):
     n = np.shape(S)[0]
 
     if n == 1:
-        distance = np.linalg.norm(point-S[0, :])
-        projection = np.transpose(S[0, :])
+        distance = np.linalg.norm(point-S[0])
+        projection = S[0].T
 
     else:
-        translatedS = []
+        translatedS = S
         for t in range(0, n):
-            translatedS[t] = S[t, :]-S[0, :]
+            translatedS[t] = S[t]-S[0]
 
-        b = np.transpose(point-S[0, :])
-        A = np.transpose(translatedS[1:])
-        top = np.multiply(np.transpose(A), A)
-        top = top+.00000001*random.random(np.shape(top))
-        bottom = np.multiply(np.transpose(A), b)
-        bottom = bottom+.00000001*random.random(np.shape(bottom))
-        alpha = np.divide(bottom, top)
-        _, pivcol = Matrx.rref(A)  # frref(A)
+        b = (point-S[0]).T
+        A = (translatedS[1:]).T
+
+        top = np.dot(A.T, A)
+        top = top+.00000001*np.random.rand(np.shape(top)[0], np.shape(top)[1])
+
+        bottom = np.dot(A.T, b)
+        bottom = bottom+.00000001*np.random.rand(np.shape(bottom)[0])
+
+        #alpha = np.divide(bottom, top)
+        alpha = np.linalg.solve(top, bottom)
+
+        # _, pivcol = Matrix.rref(A)  # frref(A)
+        __A = Matrix(A)
+        _, pivcol = __A.rref()  # frref(A)
         A = A[:, pivcol]
 
         # P = A*inv(A'*A)*A'
-        P = np.multiply(A, np.multiply(np.inv(np.multiply(np.transpose(A), A)), np.transpose(A)))
-        pprime = np.multiply(P, b)
+        P = np.dot(A, np.dot(np.linalg.inv(np.dot(A.T, A)), A.T))
+        pprime = np.dot(P, b)
 
+        ############ TODO: CURRENT ISSUE ############
         posflag = 1
         for ii in range(0, np.shape(alpha)[0]):
-            if alpha(ii) < 0:
+            if alpha[ii] < 0:
                 posflag = 0
 
         if sum(alpha) <= 1 and posflag == 1:
@@ -60,3 +67,21 @@ def distanceToSimplex(point, S):
             distance, projection = distanceToSimplex(point, S[1:, :])
 
     return distance, projection
+
+
+# SANITY TEST
+f = open('test_set1.csv', 'r')
+lines = f.read().split('\n')
+f.close()
+
+svecs = []
+for line in lines:
+    if line.strip() == "":
+        continue
+    vec = line.split(',')[1].split(' ')[:-1]
+    svecs.append(vec)
+
+vecs = np.array(svecs).astype(float)
+
+distance, projection = distanceToSimplex(vecs[0], vecs[1:])
+print(distance)
