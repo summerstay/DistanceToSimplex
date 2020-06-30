f72ff503e5d34ee85a9b37cb46608f6307c664b2
Snehesh Shrestha <snehesh@umd.edu>
2020-06-30 13:08:05 -0400

    Fixed transpose and indexing bugs, minor speed ups over loops, removed randomization, and added all 3 test cases

--- distanceToSimplex.py
+++ distanceToSimplex.py
@@ -15,21 +15,19 @@ def distanceToSimplex(point, S):
 
     if n == 1:
         distance = np.linalg.norm(point-S[0])
-        projection = S[0].T
+        projection = np.matrix(S[0]).T
 
     else:
-        translatedS = S
-        for t in range(0, n):
-            translatedS[t] = S[t]-S[0]
+        translatedS = S-S[0]        
 
-        b = (point-S[0]).T
-        A = (translatedS[1:]).T
+        b = np.matrix((point-S[0])).T
+        A = np.matrix((translatedS[1:])).T
 
         top = np.dot(A.T, A)
-        top = top+.00000001*np.random.rand(np.shape(top)[0], np.shape(top)[1])
+        #top = top+.00000001*np.random.rand(np.shape(top)[0], np.shape(top)[1])
 
         bottom = np.dot(A.T, b)
-        bottom = bottom+.00000001*np.random.rand(np.shape(bottom)[0])
+        #bottom = bottom+.00000001*np.random.rand(np.shape(bottom)[0])
 
         #alpha = np.divide(bottom, top)
         alpha = np.linalg.solve(top, bottom)
@@ -44,14 +42,15 @@ def distanceToSimplex(point, S):
         pprime = np.dot(P, b)
 
         posflag = 1
-        for ii in range(0, np.shape(alpha)[0]):
-            if alpha[ii] < 0:
-                posflag = 0
+        # for ii in range(0, np.shape(alpha)[0]):
+        #     if alpha[ii] < 0:
+        if any(alpha < 0):
+            posflag = 0
 
         if sum(alpha) <= 1 and posflag == 1:
             # projection inside simplex
             distance = np.linalg.norm(pprime-b)
-            projection = np.transpose(pprime+S[0])
+            projection = np.matrix(pprime+S[0]).T
 
         elif posflag == 0:
             Sprime = []
@@ -70,19 +69,31 @@ def distanceToSimplex(point, S):
     return distance, projection
 
 
+def test(svecs):
+    vecs = np.array(svecs).astype(float)
+    distance, projection = distanceToSimplex(vecs[0], vecs[1:])
+    return distance, projection
+
+def load_vecs(filename):
+    f = open(filename, 'r')
+    lines = f.read().split('\n')
+    f.close()
+    svecs = []
+    for line in lines:
+        if line.strip() == "":
+            continue
+        vec = line.split(',')[1].split(' ')[:-1]
+        svecs.append(vec)
+
+    return svecs
+
+all_test_cases = []
+
 # SANITY TEST
-f = open('test_set2.csv', 'r')
-lines = f.read().split('\n')
-f.close()
+all_test_cases.append([[.5,.5,.5],[0,0,0],[1,0,0],[0,1,0],[0,0,1]])
+all_test_cases.append(load_vecs('test_set1.csv'))
+all_test_cases.append(load_vecs('test_set2.csv'))
 
-svecs = []
-for line in lines:
-    if line.strip() == "":
-        continue
-    vec = line.split(',')[1].split(' ')[:-1]
-    svecs.append(vec)
-
-vecs = np.array(svecs).astype(float)
-
-distance, projection = distanceToSimplex(vecs[0], vecs[1:])
-print(distance)
+for sv in all_test_cases:
+    distance, _ = test(sv)
+    print(distance)
